63d46fb7fe835819757651a2d3d6fc5eb9f61133
anjalijha-dev <anjalijha282006@gmail.com>
2026-01-12 11:07:52 +0530

    Add basic variables and reassignment

---
+++ BasicVariables.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args){
+        //variables
+        int a = 34;
+        int b = 43;
+        double price = 25.25;
+        String name = "Tony Stark";
+
+        a = 45;
+        b = 78;
+        name = "IronMan";
+        price = 67.67;
+        System.out.print("The name is : "+ name);
+    }
+}
