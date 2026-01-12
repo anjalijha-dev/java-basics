0b44c926e1c96f468148490af0ef9087ed9a2d57
anjalijha-dev <anjalijha282006@gmail.com>
2026-01-12 12:20:00 +0530

    Add basic user input example

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Input{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        //nextInt() → reads an integer
+        //next() → reads only one word
+        //nextLine() → reads the entire line
+        System.out.println(name);
+    }
+}
