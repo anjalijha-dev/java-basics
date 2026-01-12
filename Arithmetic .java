da6e58fe036db44e408e6bd2e17580eacc7a795e
anjalijha-dev <anjalijha282006@gmail.com>
2026-01-12 11:43:24 +0530

    Update and rename SumAndDifference.java to Arithmetic .java

--- Arithmetic .java
+++ Arithmetic .java
@@ -1,11 +1,15 @@
-class SumAndDifference{
+class Arithmetic{
     public static void main(String[] args){
         //variables
-        int a = 28;
-        int b = 18;
+        int a = 20;
+        int b = 10;
         int sum = a + b;
         int diff = a - b;
+        int mul = a * b;
+        int div = a / b;
         System.out.print(sum+"\n");
         System.out.println(diff);
+        System.out.println(mul);
+        System.out.println(div);
     }
 }
