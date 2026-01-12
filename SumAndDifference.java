7caaa5a471ea6f2f9b328f6253750b4cbdc76b84
anjalijha-dev <anjalijha282006@gmail.com>
2026-01-12 11:37:52 +0530

    Add basic arithmetic operations

---
+++ SumAndDifference.java
@@ -0,0 +1,11 @@
+class SumAndDifference{
+    public static void main(String[] args){
+        //variables
+        int a = 28;
+        int b = 18;
+        int sum = a + b;
+        int diff = a - b;
+        System.out.print(sum+"\n");
+        System.out.println(diff);
+    }
+}
