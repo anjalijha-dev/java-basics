457db93853c90733240be7d49e4add4ca4562aa7
anjalijha-dev <anjalijha282006@gmail.com>
2026-01-13 11:17:20 +0530

    Add age check using if-else
    Demonstrates taking integer input using Scanner and
    using if-else conditions to check age eligibility.

---
+++ AgeCheck.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class AgeCheck{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if( age >= 18 ){
+            System.out.println("Adult!");
+        }
+        else{
+            System.out.println("Not an Adult.");
+        }
+    }
+}
