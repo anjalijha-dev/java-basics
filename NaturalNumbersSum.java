c787fd7e684d1bb069663fe19d9c532c34dcf313
anjalijha-dev <anjalijha282006@gmail.com>
2026-01-17 11:09:37 +0530

    Add sum of first n natural numbers using loop
    This program takes a number as input from the user. 
    and print the sum of the first n natural numbers using a for loop in Java.

---
+++ NaturalNumbersSum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class NaturalNumbersSum{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }System.out.print(sum);
+    }
+}
