592a4c95bdb221dadfa9712857910f22eeb4ce13
anjalijha-dev <anjalijha282006@gmail.com>
2026-01-28 14:19:13 +0530

    Add a program to form inverted half pyramid
    In this program, we create a nested loop with only two levels of nesting(for loop) because pattern questions work in a 2D plane, and then we form an inverted half pyramid using star patterns.

---
+++ Pattern4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Pattern4{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //outer loop
+        for(int i = n; i >= 1; i--){
+            //i-- => i = i - 1 (decrement operator)
+            //inner loop
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
